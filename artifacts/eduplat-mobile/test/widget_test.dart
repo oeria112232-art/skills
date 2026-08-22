4cfed884ad82f9c58cd459fb7b60791ba36bd984
oeria112232-art <oeria112232@gmail.com>
2026-08-23 01:40:56 +0300

    a

--- artifacts/eduplat-mobile/test/widget_test.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter_test/flutter_test.dart';
-import 'package:eduplat_mobile/main.dart';
-
-void main() {
-  testWidgets('EduplatApp smoke test', (WidgetTester tester) async {
-    await tester.pumpWidget(const EduplatApp());
-  });
-}
